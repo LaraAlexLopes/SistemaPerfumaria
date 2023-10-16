490525c656b510f63bbef52fb2bf0875467c3aa4
larabsi <larasfe16@gmail.com>
2023-10-16 18:51:18 -0300

    Cadastro e listagem clientes

---
+++ src/view/cadastro-clientes.js
@@ -0,0 +1,192 @@
+import React, { useState, useEffect } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+
+import Stack from '@mui/material/Stack';
+
+import Card from '../components/card';
+import FormGroup from '../components/form-group';
+
+import { mensagemSucesso, mensagemErro } from '../components/toastr';
+
+import '../custom.css';
+
+import axios from 'axios';
+import {BASE_URL_CFV} from '../config/bdCFV';
+
+function CadastroClientes() {
+  const { idParam } = useParams();
+
+  const navigate = useNavigate();
+
+  const baseURL = `${BASE_URL_CFV}/clientes`;
+
+  const [id, setId] = useState('');
+  const [nome, setNome] = useState('');
+  const [cpf, setCpf] = useState('');
+  const [email, setEmail] = useState('');
+  const [numeroTelefone, setNumeroTelefone] = useState('');
+  const [dataNascimento, setDataNascimento] = useState('');
+ 
+
+  const [dados, setDados] = React.useState([]);
+
+  function inicializar() {
+    if (idParam == null) {
+      setId('');
+      setNome('');
+      setCpf('');
+      setEmail('');
+      setNumeroTelefone('');
+      setDataNascimento('');
+     
+    } else {
+      setId(dados.id);
+      setNome(dados.nome);
+      setCpf(dados.cpf);
+      setEmail(dados.email);
+      setNumeroTelefone(dados.numeroTelefone);
+      setDataNascimento(dados.dataNascimento);
+      
+    }
+  }
+
+  async function salvar() {
+    let data = { id,  nome, cpf, email, numeroTelefone, dataNascimento };
+    data = JSON.stringify(data);
+    if (idParam == null) {
+      await axios
+        .post(baseURL, data, {
+          headers: { 'Content-Type': 'application/json' },
+        })
+        .then(function (response) {
+          mensagemSucesso(`Cliente ${nome} cadastrado com sucesso!`);
+          navigate(`/listagem-clientes`);
+        })
+        .catch(function (error) {
+          mensagemErro(error.response.data);
+        });
+    } else {
+      await axios
+        .put(`${baseURL}/${idParam}`, data, {
+          headers: { 'Content-Type': 'application/json' },
+        })
+        .then(function (response) {
+          mensagemSucesso(`Cliente ${nome} alterado com sucesso!`);
+          navigate(`/listagem-clientes`);
+        })
+        .catch(function (error) {
+          mensagemErro(error.response.data);
+        });
+    }
+  }
+
+  async function buscar() {
+    await axios.get(`${baseURL}/${idParam}`).then((response) => {
+      setDados(response.data);
+    });
+      setId(dados.id);
+      setNome(dados.nome);
+      setCpf(dados.cpf);
+      setEmail(dados.email);
+      setNumeroTelefone(dados.numeroTelefone);
+      setDataNascimento(dados.dataNascimento);
+      
+  }
+
+  const [dadosClientes, setDadosClientes] = React.useState(null);
+
+  useEffect(() => {
+    axios.get(`${BASE_URL_CFV}/clientes`).then((response) => {
+      setDadosClientes(response.data);
+    });
+  }, []);
+
+  useEffect(() => {
+    buscar(); // eslint-disable-next-line
+  }, [id]);
+
+  if (!dados) return null;
+  if (!dadosClientes) return null;
+
+  return (
+    <div className='container'>
+      <Card title='Cadastro de Clientes'>
+        <div className='row'>
+          <div className='col-lg-12'>
+            <div className='bs-component'>
+              <FormGroup label='Nome: *' htmlFor='inputNome'>
+                <input
+                  type='text'
+                  id='inputNome'
+                  value={nome}
+                  className='form-control'
+                  name='nome'
+                  onChange={(e) => setNome(e.target.value)}
+                />
+              </FormGroup>
+              <FormGroup label='CPF: *' htmlFor='inputCPF'>
+                <input
+                  type='text'
+                  maxLength='18'
+                  id='inputCpf'
+                  value={cpf}
+                  className='form-control'
+                  name='cpf'
+                  onChange={(e) => setCpf(e.target.value)}
+                />
+              </FormGroup>
+              <FormGroup label='Email: *' htmlFor='inputEmail'>
+                <input
+                  type='email'
+                  id='inputEmail'
+                  value={email}
+                  className='form-control'
+                  name='email'
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </FormGroup>
+              <FormGroup label='NumeroTelefone:' htmlFor='inputNumeroTelefone'>
+                <input
+                  type='text'
+                  id='inputNumeroTelefone'
+                  value={numeroTelefone}
+                  className='form-control'
+                  name='numeroTelefone'
+                  onChange={(e) => setNumeroTelefone(e.target.value)}
+                />
+              </FormGroup>
+              <FormGroup label='DataNascimento:' htmlFor='inputDataNascimento'>
+                <input
+                  type='text'
+                  id='iinputDataNascimento'
+                  value={dataNascimento}
+                  className='form-control'
+                  name='dataNascimento'
+                  onChange={(e) => setDataNascimento(e.target.value)}
+                />
+              </FormGroup>
+              
+              <Stack spacing={1} padding={1} direction='row'>
+                <button
+                  onClick={salvar}
+                  type='button'
+                  className='btn btn-success'
+                >
+                  Salvar
+                </button>
+                <button
+                  onClick={inicializar}
+                  type='button'
+                  className='btn btn-danger'
+                >
+                  Cancelar
+                </button>
+              </Stack>
+            </div>
+          </div>
+        </div>
+      </Card>
+    </div>
+  );
+}
+export default CadastroClientes;
